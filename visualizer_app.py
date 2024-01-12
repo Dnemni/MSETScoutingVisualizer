70ecf60fc4f9b294c223fa3e336d21390834c746
Dhruv <dhruvnemani@gmail.com>
2024-01-12 02:56:19 -0800

    push

--- visualizer_app.py
+++ visualizer_app.py
@@ -147,7 +147,7 @@ boxplot = alt.Chart(data).mark_boxplot(extent="min-max").encode(
         width=400,
         height=300
     ).configure_title(
-        text = "Team " + str(tm) + " Scoring Boxplots"
+        text = "Team " + str(tm) + " Scoring Boxplots",
         fontSize=16,
         anchor='start'
     )
