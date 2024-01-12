4166fb1f429575125f11733db41f92b14161a018
Dhruv <dhruvnemani@gmail.com>
2024-01-12 01:22:17 -0800

    minus echarts

--- visualizer_app.py
+++ visualizer_app.py
@@ -4,8 +4,6 @@ import pandas as pd
 import streamlit as st
 import tbapy
 import datetime
-import pyecharts.options as opts
-from pyecharts.charts import Grid, Boxplot, Scatter
 
 st.title("MSET Scouting Data Visualizer")
 
