e8f59c9dc16e66b7beee033c9442b44e1c896a20
Dhruv <dhruvnemani@gmail.com>
2024-01-14 11:29:30 -0800

    hmm

--- visualizer_app.py
+++ visualizer_app.py
@@ -157,9 +157,9 @@ st.header("Score Visualization")
 
 # Add teams dynamically
 sb1 = SideBarSetup()
-tm1 = sb1.tmnumIN("649")
-tmy1 = sb1.tmyrIN()
-evnt1 = sb1.tmyrevIN(0)
+tm1 = sb1.tmnumIN(0)
+tmy1 = sb1.tmyrIN(0, tm1)
+evnt1 = sb1.tmyrevIN(0, tmy1)
 teams_info.append((tm1, tmy1, evnt1))
 teams_info = []
 x = 1
